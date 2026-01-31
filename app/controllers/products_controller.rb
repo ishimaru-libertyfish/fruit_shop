7ae3f0b93318c8e9273639a1d8ef18c18f449695
ishimaru <h_ishimaru@libertyfish.co.jp>
2026-01-31 23:24:54 +0900

    gazou 20260131

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -59,7 +59,7 @@ class ProductsController < ApplicationController
 
   # ストロングパラメータで、フォームから送信されたデータを許可する
   def product_params
-    params.require(:product).permit(:name, :description, :price)
+    params.require(:product).permit(:name, :description, :price, :photo)
   end
 
   # 管理者確認メソッド
--- app/models/product.rb
+++ app/models/product.rb
@@ -3,4 +3,12 @@ class Product < ApplicationRecord
     validates :price, presence: true # 価格を必須項目に
 
     has_many :orders
+
+    # Active Storage（商品画像）
+    has_one_attached :photo
+
+    # 商品画像のサムネイルを生成
+    def thumbnail
+      photo.variant(resize_to_limit: [150, 150]).processed  # 画像を 150x150 ピクセル以内にリサイズ
+    end
 end
