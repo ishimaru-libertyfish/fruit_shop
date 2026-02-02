c656f8d61815ce9b115eb32dfd41482156209fd1
ishimaru <h_ishimaru@libertyfish.co.jp>
2026-02-02 14:06:35 +0900

    20260202

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -7,39 +7,67 @@ class OrdersController < ApplicationController
 
   # 注文内容の確認画面
   def confirm
-    @order = Order.new(order_params)          # フォームから送信された注文情報を取得
-    @product = Product.find(order_params[:product_id]) # 注文対象の商品を取得
-    @order.user = current_user  # 現在のユーザーを注文に紐付け
+  @order = Order.new(order_params)  # 注文オブジェクトを新規作成
+  @order.user_id = current_user.id  # 現在のユーザーIDを紐付け
+  # カート内のアイテムを取得
+  @cart_item = current_user.cart.cart_items.includes(:product)
+  
+  @cart_item.each do |item|
+    # build を使うとオブジェクトが作成され order_id カラムが自動的にセットされる
+    @order.order_details.build(
+      product_id: item.product.id,
+      quantity: item.quantity,
+      price: item.product.price
+    )
+  end
+  # 個々の商品の小計を配列で作る（モジュールのメソッドを使う場合）
+  item_totals = @order.order_details.map do |detail|
+    calculate_item_total(detail.price, detail.quantity)
+  end
 
-    if @order.valid?
-        @order.total_price = cal_total_price(@product.price, @order.count) # 合計金額を計算して設定
-    else
-        # バリデーションNGなら入力画面に戻る
-        render :new and return
-    end
+  # 合計金額をセット
+  @order.total_price = calculate_total_sum(item_totals)
+
+  if !@order.valid?
+      # バリデーションNGなら入力画面に戻る
+      redirect_to new_order_path, alert: "注文内容に誤りがあります。"
+  end
   end
 
 # 注文登録
   def create
     @order = Order.new(order_params)
-    @order.user_id = current_user.id # ログイン中のユーザーIDを紐付け
-    # 注文が正常に保存できた場合
-    if @order.save
-      redirect_to complete_order_path(@order)     # 登録が完了したら注文完了ページへ遷移
-    else
-      @product = Product.find(@order.product_id)
-      redirect_to new_order_path(product_id: @order.product_id)        # 注文入力へ戻る
+  @order.user_id = current_user.id # 現在のユーザーIDを紐付け(不正な値を防ぐため)
+
+  # 親の注文データを保存し、IDを確定させる
+  if @order.save
+    # カート内の商品を注文詳細として即座にデータベースに保存
+    current_user.cart.cart_items.each do |item|
+      OrderDetail.create(
+        order_id: @order.id,
+        product_id: item.product.id,
+        quantity: item.quantity,
+        price: item.product.price
+      )
     end
+    # カートのアイテムを空にする
+    current_user.cart.cart_items.destroy_all # カート内のアイテムを全て削除
+    
+    redirect_to complete_order_path(@order)     # 登録が完了したら注文完了ページへ遷移
+  else
+    redirect_to new_order_path, alert: "注文内容に誤りがあります。" 
+  end
   end
 
  # 注文完了
   def complete
     @order = Order.find(params[:id])
-    @product = Product.find(@order.product_id)
+
   end
 
   def index
-    @orders = current_user.orders.all # ユーザーに紐付いた注文履歴を表示
+    #@orders = current_user.orders.all # ユーザーに紐付いた注文履歴を表示
+    @orders = current_user.orders.order(created_at: :desc)  # ユーザーに紐付いた注文履歴を表示
   end
 
   def new
