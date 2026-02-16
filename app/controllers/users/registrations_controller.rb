921d4f29331f698ac83d5b1b3251cf7dd48fc4cc
ishimaru <h_ishimaru@libertyfish.co.jp>
2026-02-16 15:05:17 +0900

    20260216

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -52,7 +52,8 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # The path used after sign up.
    def after_sign_up_path_for(resource)
-     super(resource)
+     #super(resource)
+     mypage_path(resource)
    end
 
   # The path used after sign up for inactive accounts.
