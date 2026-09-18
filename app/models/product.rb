5be1a7f7978b96d7b3462ca8aff0d4203b72f8dc
ishimaru-libertyfish <39154900+ishimaru-libertyfish@users.noreply.github.com>
2026-09-18 03:55:58 +0000

    20260918

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,6 @@ class Product < ApplicationRecord
   # バリデーション
   validates :name, presence: true, uniqueness: true  # 商品名は必須で一意
   validates :price, presence: true                   # 価格は必須
+
+  has_many :orders
 end
